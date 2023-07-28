3fc0846d23b4b0be1b959b53e1c8f50a740716a2
listenmusic410 <listenmusic410@gmail.com>
2023-07-28 11:49:09 +0200

    t2

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/status')
+def status():
+    return jsonify({'status': 'OK'})
 
 if __name__ == '__main__':
     print('Hello World')
