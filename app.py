238e56def3b01bc6fe098f3d2e51c0e2f7496b72
sporhaberleri94 <140798231+sporhaberleri94@users.noreply.github.com>
2023-07-30 13:34:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,6 @@ def hostdetails():
 
 if __name__ == '__main__':
     print('Hello World')
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
 
 
